27f2373e95eecd25fade4c0571517badbdc0df65
Rohan Vohra <rohanvohra055@gmail.com>
2024-12-13 22:22:22 +0530

    Initial Commit

---
+++ Chat-app-Backend/dist/index.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+let userCount = 0;
+let allSockets = [];
+wss.on("connection", (socket) => {
+    userCount = userCount + 1;
+    console.log("user connected #" + userCount);
+    socket.on("message", (message) => {
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            console.log("user joined room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            });
+        }
+        if (parsedMessage.type === "chat") {
+            console.log("user wants to chat");
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
+        }
+    });
+    /*
+        socket.on("disconnect", () => {
+            allSockets = allSockets.filter(x => x != socket);
+        })*/
+});
---
+++ Chat-app-Backend/src/index.ts
@@ -0,0 +1,59 @@
+import { json } from "express";
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer( {port: 8080});
+
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+let userCount = 0;
+let allSockets: User[] = [];
+
+wss.on("connection", (socket) => {
+
+
+    
+
+    userCount = userCount + 1;
+    console.log("user connected #" + userCount);
+
+
+    socket.on("message", (message) => {
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if(parsedMessage.type === "join") {
+            console.log("user joined room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+
+            })
+        }
+
+        if(parsedMessage.type === "chat") {
+            console.log("user wants to chat");
+            let currentUserRoom = null;
+            for(let i=0; i< allSockets.length;i++) {
+                if(allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+
+            for(let i=0;i < allSockets.length;i++) {
+                if(allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+
+        }
+    })
+
+/*
+    socket.on("disconnect", () => {
+        allSockets = allSockets.filter(x => x != socket);
+    })*/
+
+})
+
